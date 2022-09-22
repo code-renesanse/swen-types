bfb92ddfb4b246eeb45f65a90ababa5cb08ebc1e
TilenOkretic <tilen.okretic@gmail.com>
2022-09-22 15:52:23 +0200

    :sparkles: fixed export default :tada:

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
-import Dictionary from "../dictionary";
-import ModelElement from "../element";
+import { Dictionary } from "../dictionary";
+import { ModelElement } from "../element";
 
-export default interface API {
+export interface API {
     imageDictionary: Dictionary<string>;
     componentDictionary: Dictionary<ModelElement>;
     modelsList: string[];
--- src/element/index.ts
+++ src/element/index.ts
@@ -1,4 +1,4 @@
-export default interface ModelElement {
+export interface ModelElement {
     instanceID: string;
     name?: string;
 }
